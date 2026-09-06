9a6f781d23f8976e8d3bcc6e312f3d16b842a325
alpeshchaudhary7 <alpeshchaudhary766@gmail.com>
2026-09-06 22:15:26 +0530

    Delete Q12__kilograms to grams .c

--- Q12__kilograms to grams .c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-float a;
-printf("enter number of kilograms= ");
-scanf("%f",&a);
-printf("number of grams of the given kilograms is: %f grams\n",a*1000);
-return 0;
-}
